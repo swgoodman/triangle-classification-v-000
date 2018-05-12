bc84f621d3b51e5958e9cd8d97f1f452dd5adb53
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-12 01:59:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,11 +13,7 @@ class Triangle
         raise TriangleError
       end
 
-      if @side_one + @side_two < @side_three || @side_three + @side_two < @side_one
-        raise TriangleError
-      end
-
-      if @side_one + @side_three <= @side_two
+      if @side_one + @side_two < @side_three || @side_three + @side_two < @side_one || @side_one + @side_three <= @side_two
         raise TriangleError
       end
 
@@ -31,9 +27,6 @@ class Triangle
         :scalene
       else @side_one != @side_two && @side_two != @side_three
         :isosceles
-      # else @side_one == 0 && @side_two == 0 && @side_three == 0 || @side_one + @side_two < @side_three || @side_three + @side_two < @side_one || @side_three + @side_one < @side_two
-      #   raise TriangleError
-      # rescue
     end
   end
 
